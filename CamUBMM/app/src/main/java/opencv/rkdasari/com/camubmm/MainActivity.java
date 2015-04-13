492969ca0bd040b47e84729b5fa4b50374702702
Radhakrishna Dasari <radhakrishna.dasari@hotmail.com>
2015-04-13 18:59:57 -0400

    Computer Vision Android projects

---
+++ CamUBMM/app/src/main/java/opencv/rkdasari/com/camubmm/MainActivity.java
@@ -0,0 +1,20 @@
+package opencv.rkdasari.com.camubmm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
